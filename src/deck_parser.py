f07ed9c1d8007665cf53ed3acc4a5522fd017627
Muhammad Iffan Hannanu <43287966+iffanh@users.noreply.github.com>
2021-04-02 23:25:26 +0200

    finishing parser

--- src/deck_parser.py
+++ src/deck_parser.py
@@ -7,14 +7,53 @@ class DeckParser():
         return 
 
     def keyword_search(self, deck_path:str, keyword:str="METRIC"):
+        """
+        This function parses through the file specified in deck_path and
+        look for the specified keyword.
+
+        Args:
+            deck_path   : (str) path to the .DATA file
+            keyword     : (str) keyword to search
+
+        Returns: 
+            hit         : (bool) whether keyword is found
+            content     : (list) content of the keyword 
+        """
+
+        hit = False
+        content = []
+
+        hit, content = self._read_file(deck_path, hit, content, keyword)
+
+        return hit, content
+
+    def _read_file(self, file_path, hit: bool, content:list, keyword:str):
         
-        if not os.path.isfile(deck_path):
-            raise Exception("%s not found" %deck_path)
+        """
+        This function parses through the file specified in deck_path and
+        look for the specified keyword. Recursively goes through the file
+        inside the INCLUDE keyword, too. 
 
+        Args:
+            file_path   : (str) path to the .DATA file
+            hit         : (bool) whether keyword is found
+            content     : (list) content of the keyword 
+            keyword     : (str) keyword to search
 
-        with open(deck_path, 'r') as f:
+        Returns: 
+            hit         : (bool) whether keyword is found
+            content     : (list) content of the keyword 
+        """
+
+        # Whether the line is inside the specified keyword
+        is_in = False
+
+        if not os.path.isfile(file_path):
+            raise Exception("%s not found" %file_path)
+
+        with open(file_path, 'r') as f:
             lines = f.readlines()
-
+       
         for line in lines:
 
             l = line.rstrip().split()
@@ -24,15 +63,28 @@ class DeckParser():
                 continue
 
             # Skip commented line
-            if l[0] == "--":
+            if l[0][0] == "-":
                 continue
             
+            # Check keyword
+            if l[0] == keyword:
+                is_in = True
+                hit = True
+                continue
 
-            print(line.rstrip().split())
+            if is_in:    
+                # Check if the next line hit another keyword
+                if l[0].isalpha():
+                    is_in = False
+                    continue
 
-        # for line in lines:
-        #     print(line.rstrip('\t').splitlines())
+                # Add the content
+                content.append(l)
 
-
-
-        return
+                # If it hits include keyword,
+                # read the include file
+                if keyword == 'INCLUDE':
+                    file_path = "%s/%s" %(file_path, l[0])
+                    hit, content = _read_file(file_path, hit, content, keyword)
+        
+        return hit, content
