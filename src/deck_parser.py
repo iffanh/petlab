eaff3f0473d00bd65578e85c3abce012a34861e5
Muhammad Iffan Hannanu <43287966+iffanh@users.noreply.github.com>
2021-04-12 22:01:36 +0200

    creating get_all_keywords function

--- src/deck_parser.py
+++ src/deck_parser.py
@@ -6,6 +6,101 @@ class DeckParser():
     def __init__(self):
         return
 
+
+    ### Reading ...
+
+    def get_all_keywords(self, file_path, content=[]):
+
+        data_folder = os.path.dirname(file_path)
+        data_file = os.path.basename(file_path)
+
+        is_include = False
+        _content = []
+
+        if not os.path.isfile(file_path):
+            raise Exception("%s not found" %file_path)
+
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+       
+        for line in lines:
+            
+            l = line.rstrip().split()
+            
+            # Skip empty lines
+            if len(l) == 0:
+                continue
+
+            # Skip commented line
+            if l[0][0] == "-":
+                continue
+
+            # If INCLUDE keyword is hit, this is for the keyword, 
+            # while the block just below this is for the content
+            if l[0] == 'INCLUDE':
+                is_include = True
+                try:
+                    content.append((keyword, _content))
+                except:
+                    # first pass
+                    pass
+                    
+                keyword = l[0]
+                _content = []
+
+                continue
+
+            # Only gets into this block if and only if it's inside include keyword. 
+            # the line that goes here is the content instead of the include keyword
+            # itself
+            if is_include:
+                _content.append(l[0])
+
+                try:
+                    content.append((keyword, _content))
+                except:
+                    # first pass
+                    pass
+
+                if l[0][0] == "'":
+                    file_path = os.path.join(data_folder, l[0][1:-1])
+                else:
+                    file_path = os.path.join(data_folder, l[0])
+
+                content = self.get_all_keywords(file_path, content)
+
+                is_include = False
+                continue
+
+            # When it hits the next keyword *besides INCLUDE)
+            if l[0].isalpha():  
+                
+                # Append the previous keyword + content
+                try:
+                    # We do not INCLUDE keyword since it's handled separately
+                    if keyword != 'INCLUDE':  
+                        content.append((keyword, _content))
+                except:
+                    # first pass
+                    pass
+                
+                #  Restart
+                keyword = l[0]
+                _content = []
+                continue
+
+            _content.append(l)
+
+        # Last keyword+content, if any
+        try:
+            content.append((keyword, _content))
+        except:
+            # first pass
+            pass
+
+        return content
+
+
     def keyword_search(self, deck_path:str, keyword:str="METRIC"):
         """
         This function parses through the file specified in deck_path and
@@ -47,7 +142,11 @@ class DeckParser():
 
         # Whether the line is inside the specified keyword
         is_in = False
+        is_include = False
 
+        data_folder = os.path.dirname(file_path)
+        data_file = os.path.basename(file_path)
+        
         if not os.path.isfile(file_path):
             raise Exception("%s not found" %file_path)
 
@@ -72,6 +171,21 @@ class DeckParser():
                 hit = True
                 continue
 
+            # If it hits include keyword,
+            # read the include file
+            if l[0] == 'INCLUDE':
+                is_include = True
+                continue
+
+            if is_include:
+                if l[0][0] == "'":
+                    file_path = os.path.join(data_folder, l[0][1:-1])
+                else:
+                    file_path = os.path.join(data_folder, l[0])
+
+                hit, content = self._read_file(file_path, hit, content, keyword)
+                is_include = False
+                
             if is_in:    
                 # Check if the next line hit another keyword
                 if l[0].isalpha():
@@ -84,7 +198,16 @@ class DeckParser():
                 # If it hits include keyword,
                 # read the include file
                 if keyword == 'INCLUDE':
-                    file_path = "%s/%s" %(file_path, l[0])
-                    hit, content = _read_file(file_path, hit, content, keyword)
-        
+
+                    if l[0][0] == "'":
+                        file_path = os.path.join(data_folder, l[0][1:-1])
+                    else:
+                        file_path = os.path.join(data_folder, l[0])
+
+                    hit, content = self._read_file(file_path, hit, content, keyword)
+
+                    
         return hit, content
+
+
+    ### Writing ...
