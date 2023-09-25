6c4b02680a9bd70e06859baf69d35d34c2c0df30
Muhammad Iffan Hannanu <iffan.hannanu@gmail.com>
2023-09-25 16:22:12 +0200

    complete the test script

--- test_script.sh
+++ test_script.sh
@@ -1,3 +1,4 @@
 
 python3 src/create_ensemble.py data/SPE1_data/SPE1_PoroPerm2_SV.json
-python3 src/run_ensemble.py /usr/bin/flow simulations/studies/IE_PoroPerm2_SV.json 
+python3 src/run_ensemble.py /usr/bin/flow simulations/studies/IE_PoroPerm2_SV.json
+python3 src/extract_ensemble.py simulations/studies/IE_PoroPerm2_SV.json 
